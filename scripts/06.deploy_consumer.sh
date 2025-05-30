52a35fbfc0019f4512cba430c0d94dea54670e1e
cristianmartella <cristian.martella@unisalento.it>
2025-05-30 20:12:50 +0200

    sidecar configuration of rustapitest; working token generation

--- scripts/06.deploy_consumer.sh
+++ scripts/06.deploy_consumer.sh
@@ -4,7 +4,7 @@ echo "*** Consumer deployment ***"
 
 consumer_root="$(builtin cd $(pwd)/..; pwd)/consumer"
 consumer_name=$(basename $consumer_root)
-consumer_identity_path="$consumer_root/consumer-identity"
+consumer_identity_path="$consumer_root/identity"
 DID_HELPER="$(pwd)/did-helper"
 
 PRIVATE_KEY="$consumer_identity_path/private-key.pem"
